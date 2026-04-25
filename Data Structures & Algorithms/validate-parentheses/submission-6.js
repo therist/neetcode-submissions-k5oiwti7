e88c735f4d68512f0bd3930e3b94c6eb916938d6
Filip Risteski <filip.risteski@gmail.com>
2026-04-25 17:43:15 -0400

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     * pendingOpen = ['(']
+     */
+    isValid(s) {
+
+        const stack = [];
+
+        for (const char of s) {
+            switch(char) {
+                case '(':
+                    stack.push(')');
+                    break;
+                case '{':
+                    stack.push('}');
+                    break;
+                case '[':
+                    stack.push(']');
+                    break;
+                default:
+                    break;
+            }
+            if (char === ')' || char === '}' || char === ']') {
+                if (stack.length === 0) return false;
+                if (stack[stack.length - 1] === char) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.length === 0;
+    }
+}
