66782a0056ca53749c5c7fff6bc7bcbe3bdc0767
Filip Risteski <filip.risteski@gmail.com>
2026-04-27 11:26:58 -0400

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+
+        let left = 0;
+        let right = (matrix.length * matrix[0].length) - 1;
+
+        while (left <= right) {
+            let mid = Math.trunc((left + right) / 2);
+
+            let row = Math.trunc(mid / matrix[0].length);
+            let column = mid % matrix[0].length;
+
+            if (matrix[row][column] === target) return true;
+            if (target > matrix[row][column]) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        return false;
+    }
+}
