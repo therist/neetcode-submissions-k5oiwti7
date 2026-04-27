9773af7c147df0d8180b1a444173d594afe8921e
Filip Risteski <filip.risteski@gmail.com>
2026-04-27 10:48:30 -0400

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    sortColors(nums) {
+        let bucket = [0, 0, 0];
+        for (let num of nums) {
+            bucket[num]++;
+        }
+        let i = 0;
+        for (let b = 0; b < bucket.length; b++) {
+            for (let k = 1; k <= bucket[b]; k++) {
+                nums[i] = b;
+                i++;
+            }
+        }
+    }
+}
